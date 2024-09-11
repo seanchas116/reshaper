dd5df77d3e5d85023f4e7371f732a122c7e49fea
Ryohei Ikegami <iofg2100@gmail.com>
2024-09-11 16:21:39 +0900

    Add simple babel plugin to add location data

---
+++ packages/demo-app/babel-plugin.js
@@ -0,0 +1,36 @@
+/**
+ * @typedef {import('@babel/core')} Babel
+ */
+
+/**
+ * @param {Babel}
+ */
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      JSXOpeningElement(path, state) {
+        // Check if the element already has a "data-location" attribute
+        const hasDataLocation = path.node.attributes.some(
+          (attr) => t.isJSXAttribute(attr) && attr.name.name === "data-location"
+        );
+
+        if (!hasDataLocation) {
+          // Get the source file path and location
+          const location = path.node.loc;
+          const filePath = state.file.opts.filename || "unknown";
+          const line = location.start.line;
+          const column = location.start.column;
+
+          // Create the "data-location" attribute
+          const dataLocationAttr = t.jsxAttribute(
+            t.jsxIdentifier("data-location"),
+            t.stringLiteral(`${filePath}#${line}:${column}`)
+          );
+
+          // Add the attribute to the element
+          path.node.attributes.push(dataLocationAttr);
+        }
+      },
+    },
+  };
+};
