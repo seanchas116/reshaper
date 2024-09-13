87d11d7fe6aaba9508a76ec208acecc48ef2877f
Ryohei Ikegami <iofg2100@gmail.com>
2024-09-13 19:10:42 +0900

    Add empty swc plugin

--- packages/demo-app/next.config.mjs
+++ packages/demo-app/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["@reshaper/swc-plugin", {}]],
+  },
+};
 
 export default nextConfig;
