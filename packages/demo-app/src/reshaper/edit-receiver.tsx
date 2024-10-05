becb18f771794b44bde1084c9675ec9ce84ecca9
Ryohei Ikegami <iofg2100@gmail.com>
2024-10-05 16:21:31 +0900

    Add receiveEdit function

--- packages/demo-app/src/reshaper/edit-receiver.tsx
+++ packages/demo-app/src/reshaper/edit-receiver.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Workspace } from "./models/workspace";
+
+function receiveEdit(workspace: Workspace) {
+  console.log("TODO: receive edit...");
+}
+
+// @ts-ignore
+window.__reshaperReceiveEdit = receiveEdit;
+
 export const EditReceiver: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
