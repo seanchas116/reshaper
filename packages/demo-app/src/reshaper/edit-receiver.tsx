dfdf19078bf58a858ec3d5f94da25ff54bdfa4b5
Ryohei Ikegami <iofg2100@gmail.com>
2024-10-05 19:10:11 +0900

    WIP: receive updates

--- packages/demo-app/src/reshaper/edit-receiver.tsx
+++ packages/demo-app/src/reshaper/edit-receiver.tsx
@@ -1,17 +1,48 @@
 "use client";
 
-import { Workspace } from "./models/workspace";
+import React, { useEffect } from "react";
+import type { Workspace } from "./models/workspace";
 
-function receiveEdit(workspace: Workspace) {
-  console.log("TODO: receive edit...", workspace);
+if (typeof window !== "undefined") {
+  window.__reshaperReceiveEdit = (workspace: Workspace) => {
+    for (const listener of editListeners) {
+      listener(workspace);
+    }
+  };
 }
 
-window.__reshaperReceiveEdit = receiveEdit;
+const editListeners = new Set<(workspace: Workspace) => void>();
 
 export const EditReceiver: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  console.log(children);
+  const [workspace, setWorkspace] = React.useState<Workspace | undefined>();
+
+  useEffect(() => {
+    const onEdit = (workspace: Workspace) => {
+      console.log("received edit", workspace);
+      setWorkspace(workspace);
+    };
+
+    editListeners.add(onEdit);
+    return () => {
+      editListeners.delete(onEdit);
+    };
+  }, []);
+
+  if (!workspace) {
+    return <>{children}</>;
+  }
+
+  for (const child of React.Children.toArray(children)) {
+    console.log(child);
+    if (React.isValidElement(child)) {
+      const reshaperLoc = child.props["data-reshaper-loc"];
+      if (reshaperLoc) {
+        // TODO: find Node by reshaperLoc and apply changes
+      }
+    }
+  }
 
   return <>{children}</>;
 };
